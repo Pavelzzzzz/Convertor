c8bc561c874068cdbb60419b704469ba0eac0d0e
Pavel Krupenin <P.Krupenin@sam-solutions.com>
2018-04-12 17:34:51 +0300

    added flyway format

---
+++ src/exception/PKException.java
@@ -0,0 +1,4 @@
+package exception;
+
+public class PKException extends Exception {
+}
--- src/service/Worker.java
+++ src/service/Worker.java
@@ -1,6 +1,8 @@
 package service;
 
 import exception.CountValues;
+import exception.PKException;
+import exception.TableNameException;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -27,9 +29,7 @@ public class Worker {
         for (int index = 0; index < fieldsName.length; index++) {
             fields.put(
                     fieldsName[index].trim(),
-                    (fieldsValues[index].trim().contains("'")) ?
-                            fieldsValues[index].trim().split("'")[1]
-                            : fieldsValues[index].trim());
+                    fieldsValues[index].trim());
         }
         return fields;
     }
@@ -47,6 +47,68 @@ public class Worker {
             result.append(field.getValue());
             result.append('\n');
         }
+        return result.toString();
+    }
+
+    public String toFlywayFormat(String tableName, Map<String, String> fields) throws TableNameException, PKException {
+        StringBuilder result = new StringBuilder();
+
+        tableName = tableName.replaceAll("\n", "");
+
+        if (tableName.split("\\.").length != 2){
+            throw new TableNameException();
+        }
+
+        if(!fields.entrySet().iterator().hasNext()){
+            throw new PKException();
+        }
+
+        StringBuilder tableNameInNewFormat = new StringBuilder();
+        tableNameInNewFormat.append('[');
+        tableNameInNewFormat.append(tableName.split("\\.")[0]);
+        tableNameInNewFormat.append("].[");
+        tableNameInNewFormat.append(tableName.split("\\.")[1]);
+        tableNameInNewFormat.append(']');
+
+        result.append("IF NOT EXISTS (SELECT * FROM ");
+        result.append(tableNameInNewFormat.toString());
+        result.append(" WHERE [");
+        result.append(fields.entrySet().iterator().next().getKey());
+        result.append("] = ");
+        result.append(fields.entrySet().iterator().next().getValue());
+        result.append(")\n");
+
+        result.append("  BEGIN\n");
+
+        result.append("    INSERT INTO ");
+        result.append(tableNameInNewFormat.toString());
+        result.append(" (\n");
+
+        String prefix = "";
+        for (Map.Entry<String, String> field
+                : fields.entrySet()){
+            result.append(prefix);
+            result.append("      [");
+            result.append(field.getKey());
+            result.append(']');
+            prefix = ",\n";
+        }
+        result.append(")\n");
+
+        result.append("VALUES (\n");
+
+        prefix = "";
+        for (Map.Entry<String, String> field
+                : fields.entrySet()){
+            result.append(prefix);
+            result.append("      ");
+            result.append(field.getValue());
+            prefix = ",\n";
+        }
+        result.append(");\n");
+
+        result.append("END");
+
         return result.toString();
     }
 
